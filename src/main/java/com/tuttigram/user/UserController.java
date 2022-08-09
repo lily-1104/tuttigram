2cff2803edd8249b7006d296d5203d4b6fa618cb
릴리\User <jon00141@naver.com>
2022-08-09 23:29:33 +0900

    lily-1104/tuttigram#4  - 로그아웃 기능 만들기 (로그아웃 과정, 세션에 로그인 정보 제거)

--- src/main/java/com/tuttigram/user/UserController.java
+++ src/main/java/com/tuttigram/user/UserController.java
@@ -1,24 +1,40 @@
 package com.tuttigram.user;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 	
+	// 로그인 화면
 	@GetMapping("/user/signin/view")
 	public String signinView() {
 		return "user/signin";
 	}
 
 	
+	// 회원 가입 화면
 	@GetMapping("/user/signup/view")
 	public String signupView() {
 		return "user/signup";
 	}
 	
 	
-	
-	
+	// 로그아웃
+	@GetMapping("/user/signout")
+	public String singOut(HttpServletRequest request) {
+		
+		// 로그아웃 과정
+		// 세션에 로그인 정보 제거
+		HttpSession session = request.getSession();
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		
+		return "redirect:/user/signin/view";
+		
+	}
 	
 }
