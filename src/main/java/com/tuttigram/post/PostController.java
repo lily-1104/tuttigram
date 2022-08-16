141b22ab055fc05916168a8d58e7c9a0b1825139
릴리\User <jon00141@naver.com>
2022-08-17 01:28:37 +0900

    lily-1104/tuttigram#5  - timeline.jsp 연결 코드 변경!

--- src/main/java/com/tuttigram/post/PostController.java
+++ src/main/java/com/tuttigram/post/PostController.java
@@ -1,22 +1,30 @@
 package com.tuttigram.post;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tuttigram.post.bo.PostBO;
+import com.tuttigram.post.model.PostDetail;
 
 @Controller
 public class PostController {
 	
+	@Autowired
+	private PostBO postBO;
+	
+	
 	@GetMapping("/post/timeline/view")
-	public String timelineView() {
+	public String timelineView(Model model) {
+		
+		List<PostDetail> postDetailList = postBO.getPostList();
+		model.addAttribute("postList", postDetailList);
+		// 데이터 조회해서 model 객체에 값을 저장한다 
 		
 		return "post/timeline";
-	}
-	
-	
-	@GetMapping("/post/timeline/vieww")
-	public String timelineVieww() {
-		
-		return "post/timelinee";
 	}
 	
 	
