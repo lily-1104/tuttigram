9d3e3cdda2458151d22e3867eb43e09883f5f42f
릴리\User <jon00141@naver.com>
2022-09-06 01:02:44 +0900

    lily-1104/tuttigram#17  - 로그인한 사용자의 userId로 '좋아요'를 눌렀는지 확인하는 기능

--- src/main/java/com/tuttigram/post/like/dao/LikeDAO.java
+++ src/main/java/com/tuttigram/post/like/dao/LikeDAO.java
@@ -26,7 +26,16 @@ public interface LikeDAO {
 	
 	
 	
+	// postId와 userId가 모두 일치하는 행의 개수
+		// dao를 만들 때에는 최대한 어떤 기능을 위한 쿼리인지 모르게 하는게 좋음, 필요한 쿼리에 대한 것만 정의 
+	public int selectCountLikeByPostUserId(
+			@Param("postId") int postId
+			, @Param("userId") int userId);
+	
+	
+	
 	// 해당하는 postId의 좋아요 모두 삭제
 	public int deleteLikeByPostId(@Param("postId") int postId);
+	
 	
 }
