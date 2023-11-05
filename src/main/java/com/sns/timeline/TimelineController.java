f6bd49ba03e29b69bfec366a61f741b8876ab79e
릴리\User <jon00141@naver.com>
2023-11-05 16:43:24 +0900

    timeline 주소 변경, PostBO Autowired 추가

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,27 +1,38 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/sns")
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+
+@RequestMapping("/timeline")
 @Controller
 public class TimelineController {
 	
 	
+	@Autowired
+	private PostBO postBO;
+	
+	
 	// 타임라인
-	// URL : http://localhost:8080/sns/timeline-view
+	// URL : http://localhost:8080/timeline/timeline-view
 	@GetMapping("/timeline-view")
 	public String timelineView(Model model) {
 		
+		List<PostEntity> postList = postBO.getPostList();
+		
+		model.addAttribute("postList", postList);
 		model.addAttribute("viewName", "timeline/timeline");
 		
 		return "template/layout";
 		
 	}
-	
-	
 	
 
 }
